9aa5b5cd2e4faa76a2ebfa97d4c6413705fc3f58
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 00:17:15 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l_height, r_height;
+	size_t l_height, r_height;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+	{
+		return (0);
+	}
 
-    l_height = binary_tree_height(tree->left);
-    r_height = binary_tree_height(tree->right);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
-    return (1 + (l_height > r_height ? l_height : r_height));
+	return (1 + (l_height > r_height ? l_height : r_height));
 }
