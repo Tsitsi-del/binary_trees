3779c273ee4873175d1d00e1aaf113bf2f35dab5
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 00:46:06 +0200

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,16 +3,16 @@
 /**
  * binary_tree_size - function to measure
  * the size of a binary tree
- * @tree - pointer to the root node of the tree
+ * @tree: pointer to the root node of the tree
  * Return: size of the binary tree, 0 if tree is NULL.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    size_t l_size = binary_tree_size(tree->left);
-    size_t r_size = binary_tree_size(tree->right);
+	size_t l_size = binary_tree_size(tree->left);
+	size_t r_size = binary_tree_size(tree->right);
 
-    return (1 + l_size + r_size);
+	return (1 + l_size + r_size);
 }
