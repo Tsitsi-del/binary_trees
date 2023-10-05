ce7dde2d53774df0822aaa03220abcf44545297e
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 01:37:15 +0200

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,35 +3,34 @@
 /**
  * binary_tree_sibling - function to find
  * the sibling of a node
- * @node - pointer to the node
+ * @node: pointer to the node
  * Return: pointer to the sibling node or NULL
  * if node is NULL or has no parent
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-    {
-        return (NULL);
-    }
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
 
-    if (node->parent->left == node)
-    {
-        return (node->parent->right);
-    }
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
 
-    if (node->parent->right == node)
-    {
-        return (node->parent->left);
-    }
-
-    return (NULL);
+	if (node->parent->right == node)
+	{
+		return (node->parent->left);
+	}
+	return (NULL);
 }
 
 /**
  * binary_tree_uncle - function to find the
  * uncle of a node in a binary tree
- * @node - pointer to the node to find the uncle
+ * @node: pointer to the node to find the uncle
  * Return: Pointer to the uncle node or NULL
  * if node is NULL, has no parent,
  * or the parent has no sibling.
@@ -39,9 +38,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-    {
-        return (NULL);
-    }
-    return binary_tree_sibling(node->parent);
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL); }
+
+	return (binary_tree_sibling(node->parent));
 }
