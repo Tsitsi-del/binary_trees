ae89cc7b9bbc3b8cecc6c737b5f9b4ccfc5c8d21
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-05 23:53:33 +0200

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,19 +2,20 @@
 
 /**
  * binary_tree_node - new binary tree node
- * @parent - pointer to the parent node of the node to create
- * @value - value to put in the node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the node
  * Return: pointer to the new node, NULL on failure
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
+
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	
+
 	if (!newNode)
 	{
-		return NULL;
+		return (NULL);
 	}
 	newNode->parent = parent;
 	newNode->n = value;
