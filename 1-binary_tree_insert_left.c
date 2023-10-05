5ea41645d582377d7dd08d228d8dbe1feea976fd
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-05 12:15:50 +0200

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,33 +7,30 @@
  * Return: pointer to the created node, NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_left(binary *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t newNode;
-	newNode = (binary_tree_t)malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
 
 	if (!parent)
-	{
+        {
 		return (NULL);
-	}
-	if (!newNode)
+        }
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
 	{
 		return (NULL);
 	}
 
 	newNode->n = value;
-	newNode->left = left;
-	newNode->right = right;
+	newNode->parent = parent;
+	newNode->left = parent->left;
+	newNode->right = NULL;
+	parent->left = newNode;
 
-	if (parent->left != NULL)
+	if (newNode->left)
 	{
-		newNode->left = parent->left;
-		parent->left = newNode;
+		newNode->left->parent = newNode;
 	}
-	else
-	{
-		parent->left = newNode;
-	}
-
 	return (newNode);
 }
