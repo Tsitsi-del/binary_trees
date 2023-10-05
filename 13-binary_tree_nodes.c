08f4c499e7172c415b29cfa5b5f30160468eeacb
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 01:04:18 +0200

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - function to count nodes
  * with at least one child in a binary tree
- * @tree - pointer to the root node of
+ * @tree: pointer to the root node of
  * the tree to count nodes
  * Return: number of internal nodes in
  * the binary tree, 0 if tree is NULL
@@ -11,13 +11,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (tree == NULL)
+	{
+		return (0); }
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-    }
-    else
-        return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	}
+	else
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
