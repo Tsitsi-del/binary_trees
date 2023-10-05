7bb07fc637305ddd85a2da4eaddaf589fa85724e
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 00:54:33 +0200

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree == NULL)
+	{
+		return (0); }
 
-	size_t l_size = binary_tree_size(tree->left);
-	size_t r_size = binary_tree_size(tree->right);
-
-	return (1 + l_size + r_size);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
