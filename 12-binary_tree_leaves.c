c1294ffc0d73d9f122ef2b960ac6f75830b028b3
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 00:58:32 +0200

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,21 +3,21 @@
 /**
  * binary_tree_leaves - Count number of leaf
  * nodes in a binary tree
- * @tree - pointer to the root node
+ * @tree: pointer to the root node
  * of the tree to count leaves
  * Return: number of leaf nodes in the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    else
-    {
-        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-    }
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
 }
