1256c1d635cd32da891519a46dabdccb07b05b3c
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-06 02:42:12 +0200

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -62,7 +62,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
-#include "binary_trees.h"
 
 /**
  * binary_tree_height - function to measure height of a binary tree
